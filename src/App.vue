e87d2181d840cab026ad4b8fab1a0d82b4480acc
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2023-03-15 14:32:14 +0100

    Add app card component

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,38 @@
 <script>
-
+import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
+import AppCard from './components/AppCard.vue';
+const apiBaseUrl = 'http://127.0.0.1:8000/api';
 
 export default {
   name: 'AppMain',
   data: () => ({
-
+    projects: [],
   }),
-  components: { AppHeader },
+  components: { AppHeader, AppCard },
   methods: {
-
+    fetchProjects() {
+      axios.get(apiBaseUrl + '/projects').then(res => {
+        this.projects = res.data;
+      })
+    }
   },
-  create: {
-
+  created() {
+    this.fetchProjects();
   }
 }
 </script>
 
 <template>
   <app-header></app-header>
+
+  <main class="container my-5">
+    <div class="row row-cols-2 g-5">
+      <div class="col" v-for="project in projects" :key="project.id">
+        <app-card :content="project"> </app-card>
+      </div>
+    </div>
+  </main>
 </template>
 
 <style lang="scss" scoped></style>
---
+++ src/components/AppCard.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    name: 'AppCard',
+    data: () => ({
+
+    }),
+    props: { content: Array }
+}
+</script>
+
+<template>
+    <div class="card h-100">
+        <div class="d-flex justify-content-center align-items-center mt-3">
+
+            <img :src="content.image" class="card-img-top img-fluid w-50" :alt="content.name">
+        </div>
+        <div class="card-body">
+            <h3 class="card-title my-3">{{ content.name }}</h3>
+            <p class="card-text">{{ content.description }}</p>
+
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
